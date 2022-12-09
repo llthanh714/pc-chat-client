105ba524e167ef1c56996a98eb9767b512001173
llthanh714@live.com <llthanh714@live.com>
2022-12-09 13:08:09 +0700

    Done chat layout

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,17 +1,21 @@
-import SendMessageForm from './SendMessageForm';
-import MessageContainer from './MessageContainer';
-import ConnectedUsers from './ConnectedUsers';
-import { Button } from 'react-bootstrap';
+import SendMessageForm from "./SendMessageForm";
+import MessageContainer from "./MessageContainer";
+import ConnectedUsers from "./ConnectedUsers";
+import { MDBBtn } from "mdb-react-ui-kit";
 
-const Chat = ({ sendMessage, messages, users, closeConnection }: any) => <div>
-    <div className='leave-room'>
-        <Button variant='danger' onClick={() => closeConnection()}>Leave Room</Button>
+const Chat = ({ sendMessage, messages, users, closeConnection }: any) => (
+  <div>
+    <div>
+      <MDBBtn className="btn-danger" onClick={() => closeConnection()}>
+        Leave Room
+      </MDBBtn>
     </div>
     <ConnectedUsers users={users} />
-    <div className='chat'>
-        <MessageContainer messages={messages} />
-        <SendMessageForm sendMessage={sendMessage} />
+    <div>
+      <MessageContainer messages={messages} />
+      <SendMessageForm sendMessage={sendMessage} />
     </div>
-</div>
+  </div>
+);
 
 export default Chat;
