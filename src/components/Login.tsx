eb4e0811961b61dbeebaa599323d6b9a8a1828c1
llthanh714@live.com <llthanh714@live.com>
2022-12-09 21:10:23 +0700

    Edit lobby.css

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -9,6 +9,7 @@ import {
   MDBRow,
 } from "mdb-react-ui-kit";
 import { useState } from "react";
+import '../App.css'
 
 const Login = ({ joinRoom }: any) => {
   const [user, setUser] = useState<string>();
